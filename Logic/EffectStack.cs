ea8d0dc3daa8aedd9ab328aa0ef2f5141676cb96
jawajoey <jfkohn@gmail.com>
2017-04-21 14:24:29 -0700

    Basic Stat Class

---
+++ Logic/EffectStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GBLib
+{
+	public class EffectStack
+	{
+
+		private object effect;
+		private int total;
+
+		public int Count { get { return total; } }
+
+		public EffectStack (object thing)
+		{
+			effect = thing;
+		}
+
+		public bool AddStack(int count=1) {
+			total += count;
+			return true;
+		}
+
+		public bool RemoveStack(int count=1) {
+			if (total == 0) {
+				return false;
+			} else if (total < count) {
+				total = 0;
+				return true;
+			} else {
+				total -= count;
+				return true;
+			}
+		}
+			
+
+	}
+}
+
---
+++ Logic/Stat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GBLib
+{
+	public class Stat
+	{
+		public int Value;
+		private int BaseValue { get; } 
+
+		public int Delta { get { return Value - BaseValue; } }
+
+		public Stat (int baseValue)
+		{
+			BaseValue = baseValue;
+			Value = BaseValue;
+		}
+
+
+		public static Stat operator +(Stat s, int i) {
+			s.Value += i;
+			return s;
+		}
+
+		public static Stat operator -(Stat s, int i) {
+			s.Value -= i;
+			return s;
+		}
+	}
+}
+
